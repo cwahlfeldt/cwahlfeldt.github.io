17030a1d92514fb25b7786bd649789f8887fe335
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-03-29 09:04:18 -0500

    init again

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,14 +3,15 @@ import Link from 'gatsby-link';
 import { placeholder } from '../common';
 
 const Work = () => (
-  <section className="work h-100 vw-100">
-    <h1 className="avenir tracked ttu pt4 near-black mb0 ph4 fw6">Work</h1>
+  <section style={{maxWidth: 1920}} className="work h-100 vw-100">
+    <h1 className="avenir ttu pt4 near-black mb0 ph4 fw6">Work</h1>
     <h5 className="avenir fw4 pb3 ph4 f7">*Work done by McKenzie Wagner, Adayana, and/or Chris Wahlfeldt.</h5>
+    <hr className="mh4 mb4 mt1" />
     <div className="flex flex-column w-100 ph4-ns ph3 pt2">
 
       {/* even */}
       <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
+        <img src={'/adams.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
         <div className="w-50-ns w-100 w-100-m">
           <p
             style={{lineHeight: 1.9}}
@@ -18,14 +19,14 @@ const Work = () => (
                avenir w-100 ph5-l order-1
             `}
           >
-            Adipisicing quia fugit ut autem ratione fugit totam aut numquam magni velit? Ducimus aliquam ratione totam voluptatem incidunt. Earum soluta laudantium laudantium consequatur eius quisquam distinctio Architecto id illum numquam!
+            <strong><Link className="link dim red pr1">Adams Village</Link></strong> is part of a much larger network of apartment complexes owned by Regency Multi-Family LLC. This site is just one of multiple sites all using a shared codebase to syncronize and maintain each site.
           </p>
         </div>
       </div>
 
       {/* odd */}
       <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
+        <img src={'/iss.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
         <div className="w-50-ns w-100 w-100-m">
           <p
             style={{lineHeight: 1.9}}
@@ -33,14 +34,14 @@ const Work = () => (
                avenir w-100 ph5-l order-0-ns order-1-m order-1
             `}
           >
-            Adipisicing quia fugit ut autem ratione fugit totam aut numquam magni velit? Ducimus aliquam ratione totam voluptatem incidunt. Earum soluta laudantium laudantium consequatur eius quisquam distinctio Architecto id illum numquam!
+            <strong><Link className="link dim blue pr1">ISS</Link></strong> provides medical and scientfic microscopes and other equipment. The site is a tool to reach their audience and sell their products. They are a global business that needed help getting there message across without being too "technical".
           </p>
         </div>
       </div>
 
       {/* even */}
       <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
+        <img src={'/villa.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
         <div className="w-50-ns w-100 w-100-m">
           <p
             style={{lineHeight: 1.9}}
@@ -55,38 +56,7 @@ const Work = () => (
 
       {/* odd */}
       <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
-        <div className="w-50-ns w-100 w-100-m">
-          <p
-            style={{lineHeight: 1.9}}
-            className={`
-               avenir w-100 ph5-l order-0-ns order-1-m order-1
-            `}
-          >
-            Adipisicing quia fugit ut autem ratione fugit totam aut numquam magni velit? Ducimus aliquam ratione totam voluptatem incidunt. Earum soluta laudantium laudantium consequatur eius quisquam distinctio Architecto id illum numquam!
-          </p>
-        </div>
-      </div>
-
-
-      {/* even */}
-      <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
-        <div className="w-50-ns w-100 w-100-m">
-          <p
-            style={{lineHeight: 1.9}}
-            className={`
-               avenir w-100 ph5-l order-1
-            `}
-          >
-            Adipisicing quia fugit ut autem ratione fugit totam aut numquam magni velit? Ducimus aliquam ratione totam voluptatem incidunt. Earum soluta laudantium laudantium consequatur eius quisquam distinctio Architecto id illum numquam!
-          </p>
-        </div>
-      </div>
-
-      {/* odd */}
-      <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
+        <img src={'/regency-corporate.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
         <div className="w-50-ns w-100 w-100-m">
           <p
             style={{lineHeight: 1.9}}
@@ -102,7 +72,7 @@ const Work = () => (
 
       {/* even */}
       <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
+        <img src={'/queue.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
         <div className="w-50-ns w-100 w-100-m">
           <p
             style={{lineHeight: 1.9}}
@@ -117,7 +87,38 @@ const Work = () => (
 
       {/* odd */}
       <div className="item flex flex-row-ns flex-column flex-column-m items-center">
-        <img src={placeholder('1440x900')} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
+        <img src={'/curb.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
+        <div className="w-50-ns w-100 w-100-m">
+          <p
+            style={{lineHeight: 1.9}}
+            className={`
+               avenir w-100 ph5-l order-0-ns order-1-m order-1
+            `}
+          >
+            Adipisicing quia fugit ut autem ratione fugit totam aut numquam magni velit? Ducimus aliquam ratione totam voluptatem incidunt. Earum soluta laudantium laudantium consequatur eius quisquam distinctio Architecto id illum numquam!
+          </p>
+        </div>
+      </div>
+
+
+      {/* even */}
+      <div className="item flex flex-row-ns flex-column flex-column-m items-center">
+        <img src={'/ileas.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-0" />
+        <div className="w-50-ns w-100 w-100-m">
+          <p
+            style={{lineHeight: 1.9}}
+            className={`
+               avenir w-100 ph5-l order-1
+            `}
+          >
+            Adipisicing quia fugit ut autem ratione fugit totam aut numquam magni velit? Ducimus aliquam ratione totam voluptatem incidunt. Earum soluta laudantium laudantium consequatur eius quisquam distinctio Architecto id illum numquam!
+          </p>
+        </div>
+      </div>
+
+      {/* odd */}
+      <div className="item flex flex-row-ns flex-column flex-column-m items-center">
+        <img src={'/blackdog.png'} className="pr3-ns pl0-ns ph3 w-50-ns w-100 w-100-m order-1-ns order-0-m order-0" />
         <div className="w-50-ns w-100 w-100-m">
           <p
             style={{lineHeight: 1.9}}

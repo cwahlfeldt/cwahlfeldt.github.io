b4c549919c443792c64d487ffd8575455d7839c2
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-03-29 11:02:30 -0500

    Updates

--- src/common/index.js
+++
@@ -1,8 +0,0 @@
-//
-// Global js and styles
-
-//import 'normalize.css';
-import './tachyons/css/tachyons.css';
-import './global.css';
-
-export const placeholder = (str) => `https://via.placeholder.com/${str}`;

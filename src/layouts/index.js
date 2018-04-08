d5ff3e5ace775be088023f577650f08ad8464486
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-04-08 14:32:16 -0500

    buiiild

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,6 +11,7 @@ const TemplateWrapper = ({ children }) => (
     <Helmet>
       <title>🤘</title>
       <link rel="stylesheet" href="https://use.typekit.net/ksq0sev.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" />
       <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
       <script src="/dom.js"></script>
     </Helmet>
--- src/pages/index.js
+++ src/pages/index.js
@@ -34,11 +34,14 @@ const IndexPage = () => (
         </div>
         <div className="w-50-ns w-100 w-100-m">
           <p
+            style={{lineHeight: 1.9}}
             className={`
-               lh-copy avenir w-100 ph3-l order-1
+               avenir w-100 ph3-l order-1 tl
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="https://www.regency-multifamily.com/communities/" target="_blank">Regency Multifamily Properties</a></strong>
+            <br />
+            Approximatley 12 sites and counting. This project allowed me to create a solid foundation for multiple sites to be created and maintained under a common codebase.
           </p>
         </div>
       </div>
@@ -50,10 +53,12 @@ const IndexPage = () => (
           <p
             style={{lineHeight: 1.9}}
             className={`
-               avenir w-100 ph3-l order-0-ns order-1-m order-1 tr-ns
+               avenir w-100 lh-copy ph3-l order-0-ns order-1-m order-1 tl
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="https://www.regency-multifamily.com/" target="_blank">Regency Multifamily</a></strong>
+            <br />
+            This project was completed to showcase what Regency Multifamily has to offer. Its also used as a tool for investors to share information.
           </p>
         </div>
       </div>
@@ -65,10 +70,12 @@ const IndexPage = () => (
           <p
             style={{lineHeight: 1.9}}
             className={`
-               avenir w-100 ph3-l order-1
+               avenir w-100 ph3-l order-1 tl
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="http://www.livingatvilla.com/" target="_blank">Villa</a></strong>
+            <br />
+            Villa is a new retirement community in my hometown. The site was a reskin of an older Wordpress installation.
           </p>
         </div>
       </div>
@@ -80,10 +87,12 @@ const IndexPage = () => (
           <p
             style={{lineHeight: 1.9}}
             className={`
-               avenir w-100 ph3-l order-0-ns order-1-m order-1 tr-ns
+               avenir w-100 ph3-l order-0-ns order-1-m order-1 tl
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="http://orig-design-iss-6028.pantheonsite.io/" target="_blank">ISS</a></strong>
+            <br />
+            This site was created to market and sell highly complex scientific equipment.
           </p>
         </div>
       </div>
@@ -95,10 +104,10 @@ const IndexPage = () => (
           <p
             style={{lineHeight: 1.9}}
             className={`
-               avenir w-100 ph3-l order-1
+               avenir w-100 ph3-l order-1 tl
             `}
           >
-            <strong><span className="f3-ns f4">Queue</span><br /><span className="near-black fw4">Used by Watson's and Blackdog in Champaign IL.</span></strong>
+            <strong><span className="f3-ns f4">Queue</span></strong><br />Used by Watson's and Blackdog in Champaign IL. The project started off as a Meteor application but has since been ported to React Native + Firebase.
           </p>
         </div>
       </div>
@@ -110,10 +119,12 @@ const IndexPage = () => (
           <p
             style={{lineHeight: 1.9}}
             className={`
-               avenir w-100 ph3-l order-0-ns order-1-m order-1 tr-ns
+               avenir w-100 ph3-l order-0-ns order-1-m order-1 tl
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="https://www.urbanaillinois.us/" target="_blank">City of Urbana</a></strong>
+            <br />
+            The City of Urbana uses this site as a tool to share and access information regarding the cities political efforts.
           </p>
         </div>
       </div>
@@ -129,6 +140,8 @@ const IndexPage = () => (
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="https://www.ileas.org/" target="_blank">ILEAS</a></strong>
+            <br />
+            The Illinois Law Enforcement Alarm System need a site to use an internal tool as well as a way to market their special police training courses.
           </p>
         </div>
       </div>
@@ -140,18 +153,23 @@ const IndexPage = () => (
           <p
             style={{lineHeight: 1.9}}
             className={`
-               avenir w-100 ph3-l order-0-ns order-1-m order-1 tr-ns
+               avenir w-100 ph3-l order-0-ns order-1-m order-1 tl
             `}
           >
             <strong><a className="link dim blue f3-ns f4" href="http://www.blackdogsmoke.com/" target="_blank">Blackdog</a></strong>
+            <br />
+            Blackdog is an award winning barbeque resturant in my hometown. This was a pro bono project from along time ago which reminds me that they need a new site!
           </p>
         </div>
       </div>
 
-      <footer className="flex mt6 justify-center">
-        <h3 className="avenir blue f3-ns f4 title tilt">Thanks</h3>
+      <footer className="flex mt6 justify-center items-center flex-column">
+        <div className="social-icons flex flex-row">
+          <a className="link dim blue f3" target="_blank" href="https://github.com/cwahlfeldt"><i className="icon-social-github"></i></a>
+          <a className="link dim blue f3 pl2 ml1 fw7" target="_blank" href="https://www.linkedin.com/in/christopher-wahlfeldt-21479474/"><i className="icon-social-linkedin"></i></a>
+        </div>
+        <p className="avenir tc fw6 f7 pa3 lh-title">All work shown was either done by me, Mckenzie Wagner, or Adayana Govt Group, and all other business' shown, respectively.</p>
       </footer>
-
 
     </div>
   </section>
